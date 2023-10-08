e2df1976cda3b66ffe88bf39a63ed950936d5baf
Akira1707 <125513222+Akira1707@users.noreply.github.com>
2023-10-08 11:23:31 +0300

    Add files via upload

---
+++ lab1_task3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+print(list_players[:int(len(list_players)/2)])
+print(list_players[int(len(list_players)/2):])
+
+
